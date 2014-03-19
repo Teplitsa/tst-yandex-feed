c96b0c8c262da88cae6f384fe0c7085e2c9bf954
Anna Ladoshkina <webdev@foralien.com>
2014-03-19 21:27:16 +0400

    fix or some invalid character in texts

--- inc/tst-yandex-feed-core.php
+++ inc/tst-yandex-feed-core.php
@@ -172,9 +172,9 @@ Allow: /yandex/news/
 		//fix for some characters block displaying feed in browser
 		$text = str_replace(
 				array('&laquo;', '&raquo;', '&ndash;', '&mdash;', '&lt;', '&gt;', '&nbsp;', '&tilde;', '&sbquo;', '&dbquo;', '&lsaquo;', 
-					'&lsquo;', '&rsquo;', '&ldquo;', '&rdquo;', '&tilde', '&rsaquo;'),
+					'&lsquo;', '&rsquo;', '&ldquo;', '&rdquo;', '&tilde', '&rsaquo;', '&minus;', '&hellip;'),
 				array('&#171;', '&#187;', "&#8212;", "&#8212;", "&#60;", "&#62;", " ", "&#126;", '&#130;', '&#132;', '&#139;', 
-					'&#145;', '&#146;', '&#147;', '&#148;', '&#152;', '&#155;'),
+					'&#145;', '&#146;', '&#147;', '&#148;', '&#152;', '&#155;', '&#45;', '&#8230;'),
 				$text);
 		
 		return $text;
