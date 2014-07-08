5e70a772832d17c4d22af1904ddc28a56901923c
Anna Ladoshkina <webdev@foralien.com>
2014-07-08 09:22:29 +0400

    Fix: correct filters for category in the feed template, correct support for empty category case

--- inc/feed.php
+++ inc/feed.php
@@ -31,21 +31,21 @@ echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?>';
 <pdalink><?php the_permalink_rss();?></pdalink>
 <description><?php the_excerpt_rss();?></description>
 <?php
-	$layf_author = apply_filters('layf_author', get_the_author()); 
+	$layf_author = apply_filters('layf_author', get_the_author(), get_the_ID()); 
 	if($layf_author):
 ?>
 <author><?php echo $layf_author; ?></author>
 <?php endif;?>
 <?php
-    $category = wp_get_post_terms(get_the_ID(), 'category');
+	$category_tax = apply_filters('layf_category_taxonomy', 'category', get_the_ID());
+    $category = wp_get_object_terms(get_the_ID(), $category_tax);
     if(count($category) > 1 && $category[0]->slug == 'uncategorized')
         $category = $category[1]->name;
     else
-        $category = $category ? reset($category)->name : '';
+        $category = $category ? reset($category)->name : '-';
 
-    $category = apply_filters('layf_category', $category);
-	if($category) :
-?>
+    $category = apply_filters('layf_category', $category, get_the_ID());
+	if($category) :?>
 <category><?php echo $category;?></category>
 <?php endif; ?>
 <?php
