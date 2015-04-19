bc1067da10d3f49fdb1756478e70806d2814cdc8
Anna Ladoshkina <webdev@foralien.com>
2015-04-19 03:01:12 +0300

    version update

--- tst-yandex-feed.php
+++ tst-yandex-feed.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Yandex.News Feed by Teplitsa
 Description: The plugin creates feed for Yandex.News service
-Version: 1.8
+Version: 1.8.1
 Author: Teplitsa
 Author URI: http://te-st.ru/
 Text Domain: layf
